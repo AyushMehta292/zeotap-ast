a70b662ac9563baaba176b576085fca257f1e36b
Ayush_Mehta <2021ugee076@nitjsr.ac.in>
2024-10-26 22:28:10 +0530

    AddRule spelling in import corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import AddRule from './Pages/AddRule/addRule';
+import AddRule from './Pages/AddRule/AddRule';
 import Home from './Pages/Home/Home';
 import Test from './Pages/Test/Test';
 import CheckRule from './Pages/CheckRule/CheckRule';
